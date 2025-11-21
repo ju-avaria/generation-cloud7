fe0e190b0f0b7b882bab4880ee8217334144af87
ju-avaria <juanavariag@gmail.com>
2025-11-21 12:47:14 -0300

    Actualizacion ejercicios 02

---
+++ cadenas.py
@@ -0,0 +1,16 @@
+miCadena = "Hola, soy una cadena de texto."
+print(miCadena)
+print(type(miCadena))
+
+primeraCadena = "Water"
+segundaCadena = "Fall"
+terceraCadena = primeraCadena + segundaCadena
+print(terceraCadena)
+
+nombre = input("Ingresa tu nombre: ")
+print(nombre)
+
+color = input("Ingresa tu color favorito: ")
+animal = input("Ingresa tu animal favorito: ")
+print(f"Tu color favorito es {color} y tu animal favorito es {animal}.")
+
---
+++ numero-interactivo.py
@@ -0,0 +1,5 @@
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+
